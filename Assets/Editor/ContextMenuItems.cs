3e06128acd892cf1180617ab247ff7a7a87eab25
irinafatu <irina.fatu91@gmail.com>
2020-11-30 10:07:42 +0200

    Added MenuItems scripts
    Added MenuItems scripts for deleting local data

---
+++ Assets/Editor/ContextMenuItems.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class ContextMenuItems
+{
+    [MenuItem("CONTEXT/Transform/Scale and Position")]
+    static void ScaleAndPosition(MenuCommand command)
+    {
+        Transform t = (Transform)command.context;
+        t.localScale = Vector3.one;
+        t.position = Vector3.zero;
+    }
+}
