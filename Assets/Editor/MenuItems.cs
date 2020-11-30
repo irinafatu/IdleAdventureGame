3fedc721a63e063f2d52684bf77b119680c20713
irinafatu <irina.fatu91@gmail.com>
2020-11-30 10:09:50 +0200

    Added Menu Items

---
+++ Assets/Editor/MenuItems.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+public class MenuItems
+{
+    [MenuItem("CUSTOM/CLEAR local data")]
+    private static void ClearLocalData()
+    {
+        foreach (var directory in Directory.GetDirectories(Application.persistentDataPath))
+        {
+            DirectoryInfo data_dir = new DirectoryInfo(directory);
+            data_dir.Delete(true);
+        }
+
+        foreach (var file in Directory.GetFiles(Application.persistentDataPath))
+        {
+            FileInfo file_info = new FileInfo(file);
+            file_info.Delete();
+        }
+
+        Debug.Log("Clear local data + " + Application.persistentDataPath.ToString());
+    }
+}
